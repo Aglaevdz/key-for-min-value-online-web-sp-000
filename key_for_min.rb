3ecae4532cc2ee90850fa14cfb858209770a2599
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-25 03:56:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  amount = " "
-  name_hash.each do |item, quanity|
+  lowest_key = nil
+  lowest_value = nil
+  name_hash.each do |key, value|
+    if lowest_value = nil || value < lowest value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key 
 end
